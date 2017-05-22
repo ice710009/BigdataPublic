736a7a520d8df1d05ce072fb54de9297162a8302
shihweiyao <shihweiyao@gmail.com>
2017-05-22 11:37:34 +0800

    20170522

--- resources/views/endowment_fund/endowment_fund1.blade.php
+++ resources/views/endowment_fund/endowment_fund1.blade.php
@@ -6,6 +6,9 @@
   				<div class="content-box-header">
 					<div class="panel-title"><h2>財務規劃報告書</h2></div>
 				</div>
+				<div class="panel-body">
+  					<h4><a href="{{ url('/endowment_fund/106report.pdf') }}"><i class="glyphicon glyphicon-download-alt"></i>106年財務規劃報告書</a></h4>
+				</div>
   				<div class="panel-body">
   					<h4><a href="{{ url('/endowment_fund/105report.pdf') }}"><i class="glyphicon glyphicon-download-alt"></i>105年財務規劃報告書</a></h4>
 				</div>
