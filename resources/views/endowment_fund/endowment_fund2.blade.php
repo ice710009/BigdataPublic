e868ba8e40ca53c83ccb5539a15fcc6f5736c63f
ice710009 <ice_cream_jl@hotmail.com>
2018-05-14 23:15:42 -0700

    20180515

--- resources/views/endowment_fund/endowment_fund2.blade.php
+++ resources/views/endowment_fund/endowment_fund2.blade.php
@@ -8,6 +8,7 @@
 				</div>
 				<div class="panel-body">
   					<h4><a href="{{ url('/endowment_fund/105fundreport.pdf') }}"><i class="glyphicon glyphicon-download-alt"></i>105年度校務基金績效報告書</a></h4>
+					<h4><a href="{{ url('/endowment_fund/106fundreport.pdf') }}"><i class="glyphicon glyphicon-download-alt"></i>106年度校務基金績效報告書</a></h4>
 				</div>
   			</div>
   			</div>
